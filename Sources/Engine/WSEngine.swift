f3dbb1e472a74e61daf4fc36a9fd8fd8118e5974
kilfu0701 <kilfu0701@gmail.com>
2022-04-08 00:43:53 +0900

    Update WSEngine.swift

--- Sources/Engine/WSEngine.swift
+++ Sources/Engine/WSEngine.swift
@@ -68,7 +68,7 @@ FrameCollectorDelegate, HTTPHandlerDelegate {
             return
         }
         
-        NSLog("connect -> ", url, request.url)
+        NSLog("connect -> %@ , %@", url, request.url)
         
         
         transport.connect(url: url, timeout: request.timeoutInterval, certificatePinning: certPinner)
